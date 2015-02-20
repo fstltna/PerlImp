4941618b05cdaa0059e7ed71b662130c30c65d95
Marisa Giancarla <fstltna@me.com>
2015-02-20 01:02:28 -0800

    Added in stub routines, and placeholder windows for most menu items - has error at this point due to definition of variables...

--- lib/perl/Imperium/Main.pm
+++ lib/perl/Imperium/Main.pm
@@ -11,6 +11,19 @@ use Gtk2::Ex::Dialogs (
 	no_separator 		=> 0 
 );
 #=======================================================================
+# Global Vars
+$DefaultServer = "empiredirectory.net";
+$DefaultServerPort = "3458";
+# Create Defs
+$ConfServerHost = $DefaultServer;
+$ConfServerPort = $DefaultServerPort;
+$ConfPlayerName = "";
+$ConfPlayerPswd = "";
+$LastSavedServerHost = "";
+$LastSavedServerPort = "";
+$LastSavedPlayerName = "";
+$LastSavedPlayerPswd = "";
+#=======================================================================
 sub new { 
 	my ($class) = @_;
 	
@@ -25,10 +38,28 @@ sub quit {
 	return unless Gtk2::Ex::Dialogs::Question->ask(
 		icon	=> q/question/,
 		title 	=> "Exit PerlImp?",
-		text 	=> "<span color='#a00'>Do you really want to quit?</span>"
+		text 	=> "<span color='#a00'>Do you really want to quit?</span>",
+		resizeable  => 0	# ZZZ What should this be?
 	);
 
 	return Gtk2->main_quit;
+}
+#======================================================================
+# Resets the server settings to default
+sub resetDefaults {
+	$ConfServerHost = $DefaultServer;
+	$ConfServerPort = $DefaultServerPort;
+	$ConfPlayerName = "";
+	$ConfPlayerPswd = "";
+}
+#======================================================================
+# Resets the server settings to last saved
+sub resetLastSaved {
+	$ConfServerHost = $LastSavedServerHost;
+	$ConfServerPort = $LastSavedServerPort;
+	$ConfPlayerName = $LastSavedPlayerName;
+	$ConfPlayerPswd = $LastSavedPlayerPswd;
+	#PrefWindow->hide()	# ZZZ How to do this?
 }
 #======================================================================
 1;
